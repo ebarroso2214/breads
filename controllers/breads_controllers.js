ba75bac579aef4e1839097325b416b4b2c8dd817
ebarroso2214 <ebarroso2214@gmail.com>
2023-03-20 20:27:23 -0400

    sending index and show data

--- controllers/breads_controllers.js
+++ controllers/breads_controllers.js
@@ -3,8 +3,10 @@ const breads = express.Router()
 const Bread = require('../models/bread.js')
 //INDEX
 breads.get('/', (req,res) => {
-    res.render('index', {
-        breads: Bread
+    res.render('index', 
+    {
+        breads: Bread,
+        title:'Breads List',
     })
     // res.send(Bread)
 })
