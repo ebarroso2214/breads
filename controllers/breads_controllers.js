67ef937099cc0fe200d11d25ccc113c921622c3d
ebarroso2214 <ebarroso2214@gmail.com>
2023-04-03 21:11:46 -0400

    added bakers pages, and listed all of the breads the baker has made currently. additonally, added dropdown menu for selecting bakers when either editing or creating new breads

--- controllers/breads_controllers.js
+++ controllers/breads_controllers.js
@@ -4,21 +4,34 @@ const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
 const seed = require('../models/seed.js')
+const Baker = require('../models/baker.js')
+
+
 //INDEX
 breads.get('/', (req,res) => {
-  Bread.find()
-    .then (foundBreads =>{
-      res.render('index', 
-      {
-        breads: foundBreads,
-        title: 'Index Page',
+  Baker.find()
+    .then(foundBakers =>{
+      Bread.find()
+      .then (foundBreads =>{
+        res.render('index', 
+        {
+          breads: foundBreads,
+          bakers:foundBakers,
+          title: 'Index Page',
+        })
       })
     })
+
 })
 
 //NEW
 breads.get('/new' , (req, res)=> {
-    res.render('new')
+    Baker.find()
+      .then(foundBakers => {
+        res.render('new',{
+          bakers:foundBakers
+        })
+      })
 })
 
 
@@ -29,6 +42,7 @@ breads.get('/new' , (req, res)=> {
 //SHOW
 breads.get('/:id', (req, res)=>{
   Bread.findById(req.params.id)
+    .populate('baker')
     .then(foundBread => {
       const bakedBy = foundBread.getBakedBy()
       console.log(bakedBy)
@@ -44,11 +58,16 @@ breads.get('/:id', (req, res)=>{
 
 //EDIT
 breads.get('/:id/edit',(req,res)=>{
-  Bread.findById(req.params.id)
-  .then(foundBread => {
-    res.render('edit', 
-    {bread: foundBread})
-  })
+  Baker.find()
+    .then(foundBakers=>{
+      Bread.findById(req.params.id)
+      .then(foundBread =>{
+        res.render('edit',{
+          bread: foundBread,
+          bakers: foundBakers
+        })
+      })
+    })
 })
 
 //UPDATE
